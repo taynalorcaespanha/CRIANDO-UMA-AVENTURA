573e51060ac157459d6b6cfcf9fed02efe3249e8
taynalorcaespanha <tayna.espanha@escola.pr.gov.br>
2025-10-30 07:37:29 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,10 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
  button.addEventListener('click', function(){
    const atual = document.querySelectorAll('.ativo');
+   const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+   
+    atual.classList.remove('ativo');
+    document.getElementById(proximoPasso).classiList.add('ativo');
+    
    })
 })
